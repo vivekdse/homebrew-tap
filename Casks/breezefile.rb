b6b6715937bb3a5f91834a3267335163399740c4
vivek-srinivasan <vivek.srinivasan@zoominfo.com>
2026-04-22 05:34:04 -0700

    breezefile 0.1.5

--- Casks/breezefile.rb
+++ Casks/breezefile.rb
@@ -1,9 +1,9 @@
 cask "breezefile" do
   arch arm: "arm64", intel: "x64"
 
-  version "0.1.4"
-  sha256 arm:   "8c635865b9e8479277b72a67d4a5377b51efa00428ffcc7a16d34bd4faa6947c",
-         intel: "76645a544a337f4b1933956386cab3611c4a00c58c54c61e3174f884b74c0859"
+  version "0.1.5"
+  sha256 arm:   "204af3e606c1a584539e24851e33187621dc71b51933a7a01f4e8ec73f971257",
+         intel: "c187570301d0137213bd527075a61fb53671163bc136ca8f442948386aedda7f"
 
   url "https://github.com/vivekdse/breezefile/releases/download/v#{version}/Breezefile-#{version}-#{arch}.dmg"
   name "Breeze File"
